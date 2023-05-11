a709a77edb768ffc0123adad401ae4b8bb7f31b9
obreira <42821882+obreira@users.noreply.github.com>
2023-05-11 13:00:37 +0100

    my new site

---
+++ Desktop/exercicio/assets/js/agency.js
@@ -0,0 +1,99 @@
+(function() {
+  "use strict"; // Start of use strict
+
+  var mainNav = document.querySelector('#mainNav');
+
+  if (mainNav) {
+
+    var navbarCollapse = mainNav.querySelector('.navbar-collapse');
+    
+    if (navbarCollapse) {
+      
+      var collapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false
+      });
+      
+      var navbarItems = navbarCollapse.querySelectorAll('a');
+      
+      // Closes responsive menu when a scroll trigger link is clicked
+      for (var item of navbarItems) {
+        item.addEventListener('click', function (event) {
+          collapse.hide();
+        });
+      }
+    }
+
+    // Collapse Navbar
+    var collapseNavbar = function() {
+
+      var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+      if (scrollTop > 100) {
+        mainNav.classList.add("navbar-shrink");
+      } else {
+        mainNav.classList.remove("navbar-shrink");
+      }
+    };
+    // Collapse now if page is not at top
+    collapseNavbar();
+    // Collapse the navbar when page is scrolled
+    document.addEventListener("scroll", collapseNavbar);
+
+    // Hide navbar when modals trigger
+    var modals = document.querySelectorAll('.portfolio-modal');
+      
+    for (var modal of modals) {
+      
+      modal.addEventListener('shown.bs.modal', function (event) {
+        mainNav.classList.add('d-none');
+      });
+        
+      modal.addEventListener('hidden.bs.modal', function (event) {
+        mainNav.classList.remove('d-none');
+      });
+    }
+  }
+
+})(); // End of use strict
+
+function smoothScroll(target, duration) {
+  var targetElement = document.querySelector(target);
+  var targetPosition = targetElement.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
